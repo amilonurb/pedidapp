44e0441add09ebfddd489abe39bae27903be6fbf
amilonurb <brunolima.18@outlook.com>
2019-05-19 04:00:06 -0300

    Add chamada ao serviço de listagem de produtos

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,12 @@
-angular.module('starter').controller('HomeController', function($scope) {
-    
-    $scope.bolos = [
-        {nome: "Só de Cenoura", preco: 18},
-        {nome: "Nutella", preco: 29},
-        {nome: "Brigadeiro", preco: 24},
-        {nome: "Açucarado", preco: 19},
-    ];
+angular.module('starter').controller('HomeController', function ($scope, produtosService) {
+    /*$scope.bolos = [
+        { nome: "Só de Cenoura", preco: 18 },
+        { nome: "Nutella", preco: 29 },
+        { nome: "Brigadeiro", preco: 24 },
+        { nome: "Açucarado", preco: 19 },
+    ];*/
+
+    produtosService.lista().then($scope, function(dados) {
+        $scope.bolos = dados;
+    });
 });
