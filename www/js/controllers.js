bd27d7c9034e5407f3073b0e85ae29bb4cad48eb
amilonurb <brunolima.18@outlook.com>
2019-05-19 04:42:43 -0300

    Seção de fazer pedido e adição e icon e splash

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -10,3 +10,49 @@ angular.module('starter').controller('HomeController', function ($scope, produto
         $scope.bolos = dados;
     });
 });
+
+angular.module('starter').controller('DetalheController', function($scope, produtosService, $stateParams) {
+    produtosService.lista().then($scope, function(dados) {
+        $scope.bolo = dados[$stateParams.boloID];
+    });
+});
+
+angular.module('starter').controller('PedidoController', function($scope, produtosService, $stateParams, $http) {
+    produtosService.lista().then($scope, function(dados) {
+        $scope.bolos = dados[$stateParams.boloID];
+    });
+
+    $scope.dados = {};
+
+    $scope.fecharPedido = function() {
+
+        $ionicLoading.show();
+
+        $http.get('http://cozinhapp.sergiolopes.org/novo-pedido', {
+            
+            params: {
+                pedido: $scope.bolo.nome,
+                info: $scope.dados.nome + ' (' + $scope.dados.telefone + ') - ' + $scope.dados.endereco
+            }
+
+        }).then(function() {
+
+            $ionicPopup.alert({
+                title: 'Pedido confirmado',
+                template: 'Daqui a pouco chega :)'
+            }).then(function() {
+                $state.go('home');
+            });
+
+        }).catch(function(erro) {
+
+            $ionicPopup.alert({
+                title: 'Erro no pedido',
+                template: erro.data + '. Deu ruim!'
+            });
+
+        }).finally(function() {
+            $ionicLoading.hide();
+        });
+    };
+});
--- www/js/routes.js
+++ www/js/routes.js
@@ -4,6 +4,18 @@ angular.module('starter').config(function($stateProvider, $urlRouterProvider) {
     $stateProvider.state('home', {
         url: '/home',
         templateUrl: 'templates/home.html',
-        controller: 'HomeController'
-    })
+        controller: 'homeController'
+    });
+
+    $stateProvider.state('detalhe', {
+        url: '/bolo/:boloID',
+        templateUrl: 'templates/detalhe.html',
+        controller: 'DetalheController'
+    });
+
+    $stateProvider.state('pedido', {
+        url: '/pedido/:boloID',
+        templateUrl: 'templates/pedido.html',
+        controller: 'PedidoController'
+    });
 });
